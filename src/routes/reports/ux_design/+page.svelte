dc361fec8627292197f3d6e56bf68c0ecea7f3f7
Parker Nix <peepnix@gmail.com>
2024-04-10 01:43:55 -0400

    Finished writing UX

--- src/routes/reports/ux_design/+page.svelte
+++ src/routes/reports/ux_design/+page.svelte
@@ -4,14 +4,19 @@
 	import Interview from '$lib/assets/Screenshot 2024-04-10 001543.png';
 </script>
 
-<h1>UX Design and A.I.</h1>
+<h1>UX Design and QoQo</h1>
 <div class="section">
 	<div class="section-card">
 		<h2>Research Goal</h2>
 		<p>
 			In this report, I will be looking to find how A.I. tools help UX designers accomplish their
-			job more easily. I am mostly looking to find how these tools can help facilitate the tedious
-			parts of UX Design, such as organizing notes, creating interview scripts, and so on.
+			job more easily with current-day technology. I am mostly looking to find how these tools can
+			help facilitate the tedious parts of UX Design, such as organizing notes, creating interview
+			scripts, and so on. To do this, I will be looking at a specific tool, QoQo, to see if it is
+			effective in organizing data, as well as ideating to find solutions and making things like
+			scripts for the researcher to use in the field. In doing this, I will be able to analyze the
+			results it gives me so that I can give a rating from 1 to 10 on how well it was able to
+			complete it's task.
 		</p>
 	</div>
 	<div class="section-card">
@@ -19,7 +24,9 @@
 		<p>
 			My research will focus on the tool, QoQo (pronounced "coco"), which is a plugin made to work
 			with Figma. It was created with the intention to automate the process of creating user journey
-			maps, generating affinity diagrams, writing interview scripts, an so on.
+			maps, generating affinity diagrams, writing interview scripts, an so on. This is a paid tool,
+			so it is somewhat inconvenient for the average user. That being said, I do feel as though the
+			potential for it to make researching and designing easier is very promising.
 		</p>
 	</div>
 </div>
@@ -40,6 +47,7 @@
 				and hit "Cluster", which starts the grouping process. After a few seconds, it had finished
 				grouping the items into three named groups.
 			</li>
+			<hr />
 			<li>
 				Make a interview script based off of some persona information and general interview topics
 				with their "Use Interview" tool. This was more of a form where it asked you to input
@@ -48,6 +56,7 @@
 				topics to ask questions about. After inputting all of that, it gave me a list of questions
 				grouped by the different topics I had entered.
 			</li>
+			<hr />
 			<li>
 				Make a "How Might We" statement with potential solutions to solve the users issues with
 				their "How Might We" tool. This tool only asked you to define a challenge you wanted to
@@ -69,17 +78,68 @@
 	<div class="section-card">
 		<h2>Evaluation</h2>
 		<p>
-			In this report, I will be looking to find how A.I. tools help UX Designers accomplish their
-			job more easily. I am mostly looking to find how these tools can help facilitate the tedious
-			parts of UX Design, such as organizing notes, creating empathy maps, and so on.
+			To evaluate the effectiveness of each part of my testing, I'll be rating the overall end
+			product each part of the test produced on a scale of 1 to 10, with 1 being completely
+			ineffective and 10 being extremely effective. Each section will have it's own definition of
+			effectiveness.
 		</p>
+		<ol>
+			<li>
+				<strong>Affinity Mapping:</strong> For affinity mapping, my effectiveness definition relies
+				mostly on whether or not the groups made sense, as well as if the insights that came out of
+				them were of quality. While the process of using it was very quick and simple, the map it
+				produced put the sticky notes into three groups, of which were not very specific and did not
+				seem particularly of interest. For instance, one group was titled "Pick Up Time", which
+				wasn't really representative of the data under it. Another was titled "Buying from food
+				truck supports local people", which would not have been useful for finding solutions for a
+				food truck. It did not encapsulate the point of the sticky notes under it, either. Overall,
+				the tool was not particularly effective.
+				<strong>Effectiveness: 3/10</strong>
+			</li>
+			<hr />
+			<li>
+				<strong>User Interview:</strong> For the user interview, my effectiveness definition was
+				reliant on if the questions produced seemed helpful towards figuring out what the user
+				really wants and needs. This tool did require a bit more manual input since it is a form and
+				not a simple selection like the affinity mapping tool, but it did give a full script with
+				valuable questions. The script includes an introduction paragraph to read before the
+				questions, which lets the interviewee know there are no wrong answers, the purpose is to
+				gather data, and that they may be recorded for team reference. Then, it has each of the
+				entered topics with a list of questions underneath. The questions are pretty
+				thought-provoking and open ended. For instance one question under the "Price" section asks,
+				"How important is the price of food when you are choosing where to eat?" Questions like this
+				do lead to great insights for a UX team to empathize with and understand the user.
+				<strong>Effectiveness: 9/10</strong>
+			</li>
+			<hr />
+			<li>
+				<strong>"How Might We" Ideation:</strong> This tool's effectiveness was rated based off of
+				whether or not the solutions it created answered the "How Might We" statement in a way that
+				was reasonable and achievable. For the results I got, I found that many of the solutions
+				were logically possible and did answer the statement. One suggestion was "Implement a mobile
+				ordering system that allows users to pre-order their food and pick it up at a designated
+				time," which was ultimately the solution our team ended up taking and found to be
+				successful. Another solution said "Create a designated seating area for users who want to
+				eat their food on the go, with convenient tables and chairs." While that would be nice for
+				customers, The likelihood that the business we were working with, a small food truck, would
+				ever be able to provide that is extremely low. This does show that the A.I. is not able to
+				carry over information between tools, as it being a small business was mentioned in other
+				parts of the test. This tool is certainly useful, but does have some issues and requires the
+				user to be vigilant still.
+				<strong>Effectiveness: 6/10</strong>
+			</li>
+		</ol>
 	</div>
 	<div class="section-card">
 		<h2>Conclusion</h2>
 		<p>
-			In this report, I will be looking to find how A.I. tools help UX Designers accomplish their
-			job more easily. I am mostly looking to find how these tools can help facilitate the tedious
-			parts of UX Design, such as organizing notes, creating empathy maps, and so on.
+			In the end, I found QoQo to be a mixed bag; some tools were effective, and gave the user very
+			helpful starting points from which they could ideate over. However, some tools either did not
+			really do what they were meant to do as a result of faulty judgement or made poor suggestions
+			due to a lack of persisting knowledge. I do feel as though tools like QoQo are immensely
+			helpful for the average UX researcher and designer, so long as they are prepared to do some
+			proofreading. It is definitely not anywhere near ready to take over jobs or anything, but some
+			polish could definitely make it a strong ally.
 		</p>
 	</div>
 </div>
