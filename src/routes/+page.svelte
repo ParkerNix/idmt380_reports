bd989a15faecce0cee67c8a18746aab148a6eb7a
Parker Nix <peepnix@gmail.com>
2024-06-12 00:21:25 -0400

    final report

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,289 @@
-<h1>Reports</h1>
-
-<h2>Please choose a report:</h2>
+<h1><span>Final Report</span></h1>
+<h2><span>Introduction</span></h2>
+<p>
+	<span
+		>During my time taking Prompt-Based Complex System Design, I was very interested to learn about
+		the different AI tools to improve the process of various different topics, especially front-end
+		development as that is the field I plan to go into after graduation. I wanted to find tools that
+		I could use in my current workplace, which is mostly focused on financial technology. To this
+		end, I explored many different tools, of which I will detail my testing of each in the following
+		paragraphs. Following this, I will go into a comparative analysis of ChatGPT and Codiumate to
+		find how they compare when measuring their ease of use, and their ability to generate test cases
+		and critique written code to improve it. I&rsquo;ll then wrap up with a reflection on how I
+		intend to move forward with the lessons I&rsquo;ve learned from my research.</span
+	>
+</p>
+<h2><span>Topic-wise Exploration</span></h2>
+<p>
+	<a href="https://idmt380-reports.vercel.app/reports"
+		><span>Click to see a list of all the reports</span></a
+	>
+</p>
+<h3>
+	<a href="https://idmt380-reports.vercel.app/reports/ux_design"><span>UX</span></a>
+</h3>
+<p>
+	<span
+		>This report examined how the AI tool QoQo can assist with UX design and research by automating
+		tasks like creating user journey maps, generating affinity diagrams, and writing interview
+		scripts. QoQo, a paid Figma plugin, was tested for its effectiveness in affinity mapping, user
+		interviews, and How Might We statement ideation. While it streamlined processes and provided
+		valuable insights for interviews, its affinity mapping had notable inaccuracies, and the How
+		Might We ideation required user vigilance due to non-specific suggestions. Overall, QoQo shows
+		promise but needs refinement to be fully reliable for UX research and design tasks. Till then, a
+		human touch is required for quality assurance.</span
+	>
+</p>
+<h3>
+	<a href="https://idmt380-reports.vercel.app/reports/ui_design"><span>UI</span></a>
+</h3>
+<p>
+	<span
+		>This report investigated Framer AI's ability to generate responsive designs for various screen
+		sizes using 3 short prompts. I wanted to test this to see if it could make creating designs
+		faster as a developer. Testing revealed that Framer AI could produce responsive layouts, but the
+		designs often felt generic and repetitive, focusing mainly on homepage-style layouts. Upon
+		trying two more detailed prompts, the AI struggled to create more varied page types like list
+		views. Overall, Framer AI was effective in organizing content for different screens but lacked
+		creativity and adherence to detailed design instructions.</span
+	>
+</p>
+<h3>
+	<a href="https://idmt380-reports.vercel.app/reports/media"><span>Media</span></a>
+</h3>
+<p>
+	<span
+		>Here I assessed the accuracy of AI transcribers on YouTube and TikTok, comparing their
+		performance across multiple videos. Both platforms showed similar error rates, with YouTube
+		having issues with lag and TikTok struggling with slang and grammatical errors. TikTok had an
+		average error rate of 4.75 (6.25 including grammar issues), while YouTube had 4.75 (6 with
+		grammar issues). I found from my research that while AI transcription is improving, it still
+		needs refinement, especially for diverse speech styles.</span
+	>
+</p>
+<h3>
+	<a href="https://idmt380-reports.vercel.app/reports/text"><span>Text</span></a>
+</h3>
+<p>
+	<span
+		>For this report, I studied the effectiveness of AI tools in generating and analyzing written
+		content. The tools GrammarlyAI and LogicBalls were tested for their ability to create coherent
+		and engaging text by giving each one prompts using different settings in their UI. I found that
+		the tools could produce readable content, but it often lacked creativity and depth, resulting in
+		repetitive or generic output. The overall conclusion is that AI text generation is promising but
+		not yet a substitute for human writers.</span
+	>
+</p>
+<h3>
+	<a href="https://idmt380-reports.vercel.app/reports/typescript"><span>Typescript</span></a>
+</h3>
+<p>
+	<span
+		>This report evaluated Codiumate, an AI tool for writing test cases in TypeScript, by testing
+		its effectiveness on withing tests for combinations of functions that are simple and complex and
+		functioning and non-functioning. Codiumate performed well across different scenarios, including
+		functioning and non-functioning code, demonstrating high accuracy and creativity in generating
+		test cases. Simple, short functions were of course easy for the tool, but even complex functions
+		didn&rsquo;t stop it from handling intricate logic and even identifying niche edge cases. The
+		overall effectiveness was rated 9.5/10. Overall, I saw Codiumate as a valuable tool for writing
+		test cases, and further more did use it in other scenarios after testing it.</span
+	>
+</p>
+<h3>
+	<a href="https://idmt380-reports.vercel.app/reports/accessibility"><span>Accessibility</span></a>
+</h3>
+<p>
+	<span
+		>In this report, I wanted to see how well ChatGPT-generated code adheres to WCAG guidelines.
+		Four prompts were used to generate a homepage for a taco restaurant, with various detail and
+		complexity, and then with specified prompting to comply with WCAG levels AA and AAA. The results
+		showed that ChatGPT's code met some guidelines but often failed in areas such as keyboard
+		navigation and proper use of heading tags. The highest score achieved was 5/6 when asked to
+		conform to level AAA, indicating that while ChatGPT can produce accessible code with specific
+		prompting, human review is still necessary to ensure full compliance.</span
+	>
+</p>
+<h3>
+	<a href="https://idmt380-reports.vercel.app/reports/seo"><span>SEO</span></a>
+</h3>
+<p>
+	<span
+		>This report investigated ChatGPT's effectiveness in teaching basic SEO principles to an
+		unfamiliar developer and improving their personal portfolio site's SEO. ChatGPT provided
+		accurate general tips that aligned with Google's SEO guidelines, recommended example websites
+		with good SEO practices, and evaluated the portfolio site, offering specific, actionable
+		improvements as prompted. The AI performed well across all tasks, scoring high in in all tests.
+		In the end, I found that ChatGPT is a valuable resource for learning and implementing basic SEO
+		strategies.</span
+	>
+</p>
+<h3>
+	<a href="https://idmt380-reports.vercel.app/reports/data_visualization"
+		><span>Data Visualization</span></a
+	>
+</h3>
+<p>
+	<span
+		>My last report evaluated ChatGPT's ability to recommend appropriate graph types and generate
+		and style React components for visualizing data using Chart.js. ChatGPT successfully recommended
+		suitable graph types and created functional React components for rendering these graphs. It was
+		able to apply specific styling to the graphs, though it required additional instructions for
+		minor adjustments. The AI showed a good understanding of graph selection and implementation, but
+		occasionally needed guidance to refine the output. This led me to conclude that ChatGPT is more
+		than able to assist in visualizing datasets, though some human oversight remains necessary for
+		optimal results.</span
+	>
+</p>
+<h2><span>Comparative Analysis</span></h2>
+<p>
+	<span
+		>I&rsquo;m now going to look at two AI tools that have been used widely to help with
+		development; ChatGPT and Codiumate. These two tools have been talked about widely in many web
+		development communities and used for creating test cases and suggesting improvements to ensure a
+		quality codebase. In this report, I&rsquo;ll be looking to see if there is a significant
+		difference between the two and which, if either, of them could be a better option for writing
+		better code.</span
+	>
+</p>
+<h3><span>Definition of the Subjects</span></h3>
 <ul>
-	<li><a href="reports/ux_design">UX Design and A.I.</a></li>
-	<li><a href="reports/ui_design">Framer AI and Responsive Design (UI Design)</a></li>
-	<li><a href="reports/media">Youtube, Tiktok, and A.I. Transcription (Media)</a></li>
-	<li><a href="reports/text">Grammarly, LogicBalls, and Text Generation (Text)</a></li>
-	<li><a href="reports/typescript">Codiumate and Test Cases (JS/TS)</a></li>
-	<li><a href="reports/accessibility">Accessibility Applied to ChatGPT (Accessibility)</a></li>
-	<li><a href="reports/seo">ChatGPT's ability to teach SEO Basics (SEO)</a></li>
-	<li><a href="reports/data_visualization">ChatGPT and Graphs (Data Visualization)</a></li>
+	<li>
+		<span
+			>ChatGPT: ChatGPT is a well-known prompt-based A.I. that can do many things, generating code
+			included. It&rsquo;s not specifically made for coding, but has been widely used to improve the
+			development process.</span
+		>
+	</li>
+	<li>
+		<span
+			>Codiumate: Codiumate is a development-focused AI tool that seeks to help create test cases
+			and suggest improvements to written code. It is an extension available for code editors like
+			Visual Studio Code.</span
+		>
+	</li>
 </ul>
+<h3><span>Criteria</span></h3>
+<ul>
+	<li>
+		<span
+			>Ease of Use: How easy is it to get an end product? Do you need an account? Are there any
+			extra seteps or potential issues a user could have trying to get a solution?</span
+		>
+	</li>
+	<li>
+		<span
+			>Suggesting: How many suggestions can it give? Can it critique various aspects of the code
+			(readability, syntax, accessibility)?</span
+		>
+	</li>
+	<li>
+		<span
+			>Test Cases: Can the tool make test cases? Are they expansive and test a large variety of
+			potential issues?</span
+		>
+	</li>
+</ul>
+<h3><span>Comparison</span></h3>
+<p><strong>Ease of Use:</strong></p>
+<ul>
+	<li>
+		<span
+			>ChatGPT: ChatGPT is not accessible from VSC, so there is a bit of window switching that has
+			to happen. However, it is not necessary to have an account to use it, only to save chat logs.
+			It will generate test cases</span
+		>
+	</li>
+	<li>
+		<span
+			>Codiumate: As it is an extension for VSC, you can access it by simply highlighting your code,
+			right clicking, and asking it to generate tests, summarize the code, and more. It can be set
+			to make test cases for different packages (such as React Testing Library), but it must be set
+			up in the repository and linked in the extension settings to run test cases. It can only
+			generate them without it.</span
+		>
+	</li>
+</ul>
+<p><strong>Suggesting:</strong></p>
+<ul>
+	<li>
+		<span
+			>ChatGPT: Upon prompting with code, it is able to give several suggestions (5 from one snippet
+			function that loops through each item in one array and tries to find a matching string in
+			another and deletes the string from both). It looks for improvements such as where comments
+			can be added, where logic can be simplified, and where it can add in error handling.</span
+		>
+	</li>
+	<li>
+		<span
+			>Codiumate: Codiumate will popup with a sidebar chat and give you several suggestions (4 from
+			the same prompt, but there is a button to generate more). From here, you can choose a
+			suggestion to get more details on it, see a code preview, or hit a checkbox to have it
+			implement the suggestion into a copy of your code, which can be reviewed and then put in
+			palace of your original code.</span
+		>
+	</li>
+</ul>
+<p><strong>Test Cases:</strong></p>
+<ul>
+	<li>
+		<span
+			>ChatGPT: It was able to generate several test cases (5 from one snippet function that added
+			two numbers together), while also explaining the hooks it used (such as the
+			&ldquo;beforeEach&rdquo; hook in React Testing Library). These were extensive and covered a
+			variety of cases(adding negative numbers, adding 0, etc.).</span
+		>
+	</li>
+	<li>
+		<span
+			>Codiumate: It was able to generate several test cases (2 from the same snippet). The first
+			test is accurate and tests the main purpose of the function. The second test tries to use the
+			function with an empty array, which is not something I would immediately think to test but is
+			a good security measure.</span
+		>
+	</li>
+</ul>
+<h3><span>Conclusion</span></h3>
+<p>
+	<span
+		>When comparing ChatGPT and Codiumate in terms of assisting with coding, it is clear that both
+		tools are strong contenders. There are a few nuances, however:</span
+	>
+</p>
+<ul>
+	<li>
+		<span
+			>ChatGPT may be more suited to someone who isn&rsquo;t as familiar with coding, as I feel it
+			was a lot more explanatory and offered breakdowns of what, how, and why it used different
+			hooks, methods, and so on. Being accessible from the web and many devices, developers can get
+			help pretty much anywhere.</span
+		>
+	</li>
+	<li>
+		<span
+			>Codiumate seems to be a better choice for developers who have a good handle on coding, but
+			might just want a second opinion or help writing tedious test cases faster. It&rsquo;s
+			availability on code editors like VSC is a big benefit.</span
+		>
+	</li>
+</ul>
+<p>
+	<span
+		>In the end, the choice of whether to code with ChatGPT or Codiumate as a tool will largely come
+		down to your experience level and priorities.</span
+	>
+</p>
+<h2><span>Conclusion</span></h2>
+<p>
+	<span
+		>At the end of my research, I feel I have deepened my understanding of AI and where it is
+		headed. I learned that, for most fields, AI is still largely in need of human supervision to
+		ensure that the outputs it gives are of quality. I was largely surprised to find that even areas
+		I thought were pretty developed, such as text generation, are lacking a sense of realism in that
+		they do not feel natural or similar to what a human might create. I think, in the future, we
+		will see AI train with more and more content, which will in turn make them better creators. I
+		would just hope that the training comes from many diverse sources. As seen in my transcription
+		research, many are not trained to recognize varied forms of speech. This is a big concern, and
+		could lead to AI becoming somewhat discriminate. Therefore, it&rsquo;s crucial that we review
+		not only the tools themselves, but perhaps also how they are created.</span
+	>
+</p>
